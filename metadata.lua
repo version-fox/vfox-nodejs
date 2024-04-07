7b6b216eb8a4d6deb7accf2809baa51e6c0e7a63
lihan <lihan@apache.org>
2024-04-07 16:54:15 +0800

    release v0.2.0

--- metadata.lua
+++ metadata.lua
@@ -5,7 +5,7 @@ PLUGIN = {}
 --- Plugin name
 PLUGIN.name = "nodejs"
 --- Plugin version
-PLUGIN.version = "0.1.1"
+PLUGIN.version = "0.2.0"
 --- Plugin repository
 PLUGIN.homepage = "https://github.com/version-fox/vfox-nodejs"
 --- Plugin license
