1aba0e856249cce1499c5206e4fd4d87d751b9a0
野声 <lijiacheng.ljc@antgroup.com>
2024-04-09 10:35:17 +0800

    Release 0.2.2

--- metadata.lua
+++ metadata.lua
@@ -5,7 +5,7 @@ PLUGIN = {}
 --- Plugin name
 PLUGIN.name = "nodejs"
 --- Plugin version
-PLUGIN.version = "0.2.1"
+PLUGIN.version = "0.2.2"
 --- Plugin repository
 PLUGIN.homepage = "https://github.com/version-fox/vfox-nodejs"
 --- Plugin license
