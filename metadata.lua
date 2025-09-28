6765789ea098a547dc79d7eae2f70c98a8aefb58
jiacheng.li <jiacheng.li@bytedance.com>
2025-09-28 14:53:17 +0800

    release v0.4.0

--- metadata.lua
+++ metadata.lua
@@ -5,7 +5,7 @@ PLUGIN = {}
 --- Plugin name
 PLUGIN.name = "nodejs"
 --- Plugin version
-PLUGIN.version = "0.3.0"
+PLUGIN.version = "0.4.0"
 --- Plugin repository
 PLUGIN.homepage = "https://github.com/version-fox/vfox-nodejs"
 --- Plugin license
